3ea873ccbca94410be19e3d9c9f2c39fc0cce4be
thierry <thierrypoupon@hotmail.com>
2021-04-14 20:10:30 +0200

    updated file

--- api/users.js
+++ api/users.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router()
 
-const User = require('../models/User');
+const User = require('../models/user');
+// const User = require('../models/user');
 
 router.get('/', (req, res) => {
     User.find()
