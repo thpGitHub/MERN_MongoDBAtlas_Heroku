67f54891bbe52d44957a51b26516d86fcefc8d2e
thierry <thierrypoupon@hotmail.com>
2021-04-23 19:36:28 +0200

    updated file

--- database.js
+++ database.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
-// const connection = "mongodb+srv://username:<password>@<cluster>/<database>?retryWrites=true&w=majority";
-const connection = "mongodb+srv://thp_adm:admthp@cluster0.q8dcp.mongodb.net/test?retryWrites=true&w=majority";
+require('dotenv').config();
+
+const connection = process.env.DB_HOST_ATLAS;
 mongoose.connect(connection,{ useNewUrlParser: true,
                               useUnifiedTopology: true,
                               useFindAndModify: false})
     .then(() => console.log("Database Connected Successfully"))
     .catch(err => console.log(err));
-
-    //mongodb+srv://thp_adm:<password>@cluster0.q8dcp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
